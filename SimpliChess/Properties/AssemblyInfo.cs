b8ffea4d6048d23e083fafb04816bad09198996d
Emlin <49819627+R4YVEN@users.noreply.github.com>
2022-06-12 20:30:46 +0200

    Changed name

--- SimpliChess/Properties/AssemblyInfo.cs
+++ SimpliChess/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Allgemeine Informationen über eine Assembly werden über die folgenden
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("SchissEngineCS")]
+[assembly: AssemblyTitle("SimpliChess")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("7476")]
-[assembly: AssemblyProduct("SchissEngineCS")]
+[assembly: AssemblyProduct("SimpliChess")]
 [assembly: AssemblyCopyright("Copyright © 7476 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
